4fcea455b4849041852da3b2d4fa78931794c588
Chas Emerick <chas@cemerick.com>
2013-08-05 14:31:53 -0400

    put test runners in default temp dir, not target

--- src/clojurescript/test/plugin.clj
+++ src/clojurescript/test/plugin.clj
@@ -7,11 +7,9 @@
   "Modify command vector annotated with ^:cljs-test so that they receive the
 location of a runner.js copy as second argument."
   [project]
-  (let [target-folder (file (or (:target-path project) "target/"))]
-    (.mkdirs target-folder)
-    (let [runner (File/createTempFile "test-runner" ".js" target-folder)
-          runner-path (.getAbsolutePath runner)]
-      (.deleteOnExit runner)
-      (copy (slurp (resource "cemerick/cljs/test/runner.js")) runner)
-      (update-in project [:cljsbuild :test-commands]
-                 #(postwalk-replace {:cljs.testrunner runner-path} %)))))
+  (let [runner (File/createTempFile "test-runner" ".js")
+        runner-path (.getAbsolutePath runner)]
+    (.deleteOnExit runner)
+    (copy (slurp (resource "cemerick/cljs/test/runner.js")) runner)
+    (update-in project [:cljsbuild :test-commands]
+               #(postwalk-replace {:cljs.testrunner runner-path} %))))

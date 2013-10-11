09979affba8931c18276a45321b75a388cd7f9a2
Chas Emerick <chas@cemerick.com>
2013-10-11 16:17:14 -0400

    enhance plugin to add itself as a dependency for simpler config

--- src/clojurescript/test/plugin.clj
+++ src/clojurescript/test/plugin.clj
@@ -3,13 +3,34 @@
             [clojure.walk :refer (postwalk-replace)])
   (:import java.io.File))
 
+(def ^:private version
+  (let [[_ coords version] (-> (or (resource "META-INF/leiningen/com.cemerick/clojurescript.test/project.clj")
+                             ; this should only ever come into play when testing clojurescript.test itself
+                             "project.clj")
+                         slurp
+                         read-string)]
+    (assert (= coords 'com.cemerick/clojurescript.test)
+            (str "Something very wrong, could not find clojurescript.test's project.clj, actually found: "
+                 coords))
+    (assert (string? version)
+            (str "Something went wrong, version of clojurescript.test is not a string: "
+                 version))
+    version))
+
 (defn middleware
-  "Modify command vector annotated with ^:cljs-test so that they receive the
-location of a runner.js copy as second argument."
+  "Does two things:
+
+1. Modify all :cljsbuild :test-command vectors, swapping :cljs.testrunner keyword
+for string path to the packaged runner.js.
+2. Add [com.cemerick/clojurescript-test \"CURRENT_VERSION\"] as a project dependency."
   [project]
   (let [runner (File/createTempFile "test-runner" ".js")
         runner-path (.getAbsolutePath runner)]
     (.deleteOnExit runner)
     (copy (slurp (resource "cemerick/cljs/test/runner.js")) runner)
-    (update-in project [:cljsbuild :test-commands]
-               #(postwalk-replace {:cljs.testrunner runner-path} %))))
+    (-> project
+        (update-in [:dependencies]
+                   (fnil into [])
+                   [['com.cemerick/clojurescript.test version]])
+        (update-in [:cljsbuild :test-commands]
+                   #(postwalk-replace {:cljs.testrunner runner-path} %)))))

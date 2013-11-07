2a9679891205a37eccfb7e0410d5da07f06eb01f
Chas Emerick <chas@cemerick.com>
2013-11-07 04:55:01 -0500

    0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cemerick/clojurescript.test "0.2.0"
+(defproject com.cemerick/clojurescript.test "0.2.1-SNAPSHOT"
   :description "Port of clojure.test targeting ClojureScript."
   :url "http://github.com/cemerick/clojurescript.test"
   :license {:name "Eclipse Public License"
@@ -33,7 +33,7 @@
              :dev {:plugins [[com.cemerick/austin "0.1.1"]]}
              ; self-reference and chained `lein install; lein test` invocation
              ; needed to use the project as its own plugin. Leiningen :-(
-             :self-plugin [:default {:plugins [[com.cemerick/clojurescript.test "0.2.0"]]}]}
+             :self-plugin [:default {:plugins [[com.cemerick/clojurescript.test "0.2.1-SNAPSHOT"]]}]}
 
   :aliases  {"cleantest" ["with-profile" "self-plugin" ;"self-plugin:self-plugin,latest"
                           "do" "clean," "test"]

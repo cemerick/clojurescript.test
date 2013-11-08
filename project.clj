ff84ba547bac70cc525e379aad6c7da6c20a1ef2
Chas Emerick <chas@cemerick.com>
2013-11-08 11:11:52 -0500

    test against cljs 2014 in `latest` profile, drop cljsbuild hooks

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
 
   :plugins [[lein-cljsbuild "1.0.0-alpha1"]]
 
-  :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:source-paths ["src" "test"]
                         :compiler {:output-to "target/cljs/whitespace.js"
                                    :optimizations :whitespace
@@ -29,14 +28,14 @@
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :profiles {:latest {:dependencies [[org.clojure/clojure "1.5.1"]
-                                     [org.clojure/clojurescript "0.0-1934"]]}
-             :dev {:plugins [[com.cemerick/austin "0.1.1"]]}
+                                     [org.clojure/clojurescript "0.0-2014"]]
+                      :plugins [[com.cemerick/austin "0.1.3"]]}
              ; self-reference and chained `lein install; lein test` invocation
              ; needed to use the project as its own plugin. Leiningen :-(
              :self-plugin [:default {:plugins [[com.cemerick/clojurescript.test "0.2.1-SNAPSHOT"]]}]}
 
-  :aliases  {"cleantest" ["with-profile" "self-plugin" ;"self-plugin:self-plugin,latest"
-                          "do" "clean," "test"]
+  :aliases  {"cleantest" ["with-profile" "self-plugin:self-plugin,latest"
+                          "do" "clean," "test," "cljsbuild" "test"]
              "release" ["do" "clean," "deploy" "clojars," "deploy" "releases"]}
 
   :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}

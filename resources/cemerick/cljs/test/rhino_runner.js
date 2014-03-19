b565b1d7ddc8f7fc7f8f4c2c3c7cb8fd1b54c933
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-03-19 00:59:33 +0000

    Fix Rhino runner to make it work with async scripts and tests. Close #54.

--- resources/cemerick/cljs/test/rhino_runner.js
+++ resources/cemerick/cljs/test/rhino_runner.js
@@ -1,18 +1,49 @@
+var setTimeout, clearTimeout, setInterval, clearInterval;
+
+(function () {
+    var executor = new java.util.concurrent.Executors.newScheduledThreadPool(1);
+    var counter = 1;
+    var ids = {};
+
+    setTimeout = function (fn,delay) {
+        var id = counter++;
+        var runnable = new JavaAdapter(java.lang.Runnable, {run: fn});
+        ids[id] = executor.schedule(runnable, delay, java.util.concurrent.TimeUnit.MILLISECONDS);
+        return id;
+    }
+
+    clearTimeout = function (id) {
+        ids[id].cancel(false);
+        executor.purge();
+        delete ids[id];
+    }
+
+    setInterval = function (fn,delay) {
+        var id = counter++;
+        var runnable = new JavaAdapter(java.lang.Runnable, {run: fn});
+        ids[id] = executor.scheduleAtFixedRate(runnable, delay, delay, java.util.concurrent.TimeUnit.MILLISECONDS);
+        return id;
+    }
+
+    clearInterval = clearTimeout;
+
+})()
+
 arguments.forEach(function (arg) {
     if (new java.io.File(arg).exists()) {
-      try {
-        load(arg);
-      } catch (e) {
-        print("Error in file: \"" + arg + "\"");
-        print(e);
-      }
+        try {
+            load(arg);
+        } catch (e) {
+            print("Error in file: \"" + arg + "\"");
+            print(e);
+        }
     } else {
-      try {
-        eval("(function () {" + arg + "})()");
-      } catch (e) {
-        print("Could not evaluate expression: \"" + arg + "\"");
-        print(e);
-      }
+        try {
+            eval("(function () {" + arg + "})()");
+        } catch (e) {
+            print("Could not evaluate expression: \"" + arg + "\"");
+            print(e);
+        }
     }
 });
 
@@ -23,6 +54,6 @@ cemerick.cljs.test.set_print_fn_BANG_(function(x) {
 });
 
 var results = cemerick.cljs.test.run_all_tests();
-var success = cemerick.cljs.test.successful_QMARK_(results);
-
-java.lang.System.exit(success ? 0 : 1);
+cemerick.cljs.test.on_testing_complete(results, function () {
+    java.lang.System.exit(cemerick.cljs.test.successful_QMARK_(results) ? 0 : 1);
+});

64459d4efa6d83a67b7ccbafa0bdf03e5df4a965
Chas Emerick <chas@cemerick.com>
2014-12-12 14:40:46 -0500

    generate a static HTML page when testing under phantom/slimer Avoids JS context issues when scripts are loaded dynamically via page.injectJs, etc (https://groups.google.com/forum/#!topic/slimerjs/8MVNd7BSZBQ)

--- resources/cemerick/cljs/test/runner.js
+++ resources/cemerick/cljs/test/runner.js
@@ -5,66 +5,82 @@ var p = require('webpage').create();
 var fs = require('fs');
 var sys = require('system');
 
+// this craziness works around inscrutable JS context issues when tests being
+// run use iframes and such; rather than injecting or eval'ing test scripts and
+// expressions, dump them all into a static HTML file and everything will be
+// guaranteed to work.
+
+var html = "";
+var pagePath = sys.args[0] + ".html";
+
 for (var i = 1; i < sys.args.length; i++) {
     if (fs.exists(sys.args[i])) {
-        if (!p.injectJs(sys.args[i])) throw new Error("Failed to inject " + sys.args[i]);
+	html += "<script>//<![CDATA[\n" + fs.read(sys.args[i]) + "\n//]]></script>";
     } else {
-	p.evaluate(function (expr) { eval(expr); }, sys.args[i]);
+	html += "<script>//<![CDATA[\n" + sys.args[i] + "\n//]]></script>";
     }
 }
 
-p.onError = function(msg) {
-  var haveCljsTest = p.evaluate(function() {
-    return (typeof cemerick !== "undefined" &&
-        typeof cemerick.cljs !== "undefined" &&
-        typeof cemerick.cljs.test !== "undefined" &&
-        typeof cemerick.cljs.test.run_all_tests === "function");
-  });
+html = "<html><head>" + html + "</head><body></body></html>";
+fs.write(pagePath, html, 'w');
 
-  if  (haveCljsTest) {
-      console.error(msg);
-  } else {
-      var messageLines = [
-          "",
-          "ERROR: cemerick.cljs.test was not required.",
-          "",
-          "You can resolve this issue by ensuring [cemerick.cljs.test] appears",
-          "in the :require clause of your test suite namespaces.",
-          "Also make sure that your build has actually included any test files.",
-          ""
-      ];
-      console.error(messageLines.join("\n"));
-      phantom.exit(1);
-  }
-};
+p.open("file://" + pagePath, function () {
+    fs.remove(pagePath);
 
-p.onCallback = function (x) {
-  var line = x.toString();
-  if (line !== "[NEWLINE]") {
-    console.log(line.replace(/\[NEWLINE\]/g, "\n"));
-  }
-};
+    p.onError = function(msg) {
+	var haveCljsTest = p.evaluate(function() {
+	    return (typeof cemerick !== "undefined" &&
+		    typeof cemerick.cljs !== "undefined" &&
+		    typeof cemerick.cljs.test !== "undefined" &&
+		    typeof cemerick.cljs.test.run_all_tests === "function");
+	});
 
-p.evaluate(function () {
-  cemerick.cljs.test.set_print_fn_BANG_(function(x) {
-    // using callPhantom to work around https://github.com/laurentj/slimerjs/issues/223
-    window.callPhantom(x.replace(/\n/g, "[NEWLINE]")); // since console.log *itself* adds a newline
-  });
+	console.error(msg);
+
+	if (!haveCljsTest) {
+	    var messageLines = [
+		"",
+		"ERROR: cemerick.cljs.test was not required.",
+		"",
+		"You can resolve this issue by ensuring [cemerick.cljs.test] appears",
+		"in the :require clause of your test suite namespaces.",
+		"Also make sure that your build has actually included any test files.",
+		""
+	    ];
+	    console.error(messageLines.join("\n"));
+	    phantom.exit(1);
+	}
+    };
+
+    p.onCallback = function (x) {
+	var line = x.toString();
+	if (line !== "[NEWLINE]") {
+	    console.log(line.replace(/\[NEWLINE\]/g, "\n"));
+	}
+    };
+
+    p.evaluate(function () {
+	cemerick.cljs.test.set_print_fn_BANG_(function(x) {
+	    // using callPhantom to work around https://github.com/laurentj/slimerjs/issues/223
+	    window.callPhantom(x.replace(/\n/g, "[NEWLINE]")); // since console.log *itself* adds a newline
+	});
+    });
+
+    // p.evaluate is sandboxed, can't ship closures across;
+    // so, a bit of a hack, better than polling :-P
+    var exitCodePrefix = "phantom-exit-code:";
+    p.onAlert = function (msg) {
+	var exit = msg.replace(exitCodePrefix, "");
+	if (msg != exit) phantom.exit(parseInt(exit));
+    };
+
+    p.evaluate(function (exitCodePrefix) {
+	var results = cemerick.cljs.test.run_all_tests();
+	//console.log(results);
+	cemerick.cljs.test.on_testing_complete(results, function () {
+	    window.alert(exitCodePrefix +
+			 (cemerick.cljs.test.successful_QMARK_(results) ? 0 : 1));
+	});
+    }, exitCodePrefix);
+
 });
-
-// p.evaluate is sandboxed, can't ship closures across;
-// so, a bit of a hack, better than polling :-P
-var exitCodePrefix = "phantom-exit-code:";
-p.onAlert = function (msg) {
-  var exit = msg.replace(exitCodePrefix, "");
-  if (msg != exit) phantom.exit(parseInt(exit));
-};
-
-p.evaluate(function (exitCodePrefix) {
-  var results = cemerick.cljs.test.run_all_tests();
-  //console.log(results);
-  cemerick.cljs.test.on_testing_complete(results, function () {
-      window.alert(exitCodePrefix +
-        (cemerick.cljs.test.successful_QMARK_(results) ? 0 : 1));
-  });
-}, exitCodePrefix);
